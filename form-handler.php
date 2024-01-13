f7088ab6d22c56a573268ffa3049ad2ce8135372
Abdullah Shakir <135498279+ChaudaryAbdullah@users.noreply.github.com>
2024-01-13 22:12:32 +0500

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,22 @@
+﻿<?php
+$name = $_POST['Name'];
+$email = $_POST['Email'];
+$subject = $_POST['Subject'];
+$message = $_POST['Message'];
+
+$email_from = '';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n"."Email: $email.\n"."Subject: $subject.\n"."Message:$message";
+
+$to = 'abdullahshakir226@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $email \r\n";
+
+mail($to,$subject,$email_body,$headers);
+
+header("Location: contact.html");
+?>
